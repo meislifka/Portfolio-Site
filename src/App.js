ec93b72422e49dfee4f6cc7edb92c905dd5c7f7d
meislifka <meislifka@gmail.com>
2024-01-10 23:53:54 -0500

    Created pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import Navbar from './components/NavBar';
+import { AboutMe } from './components/AboutMe';
+import { Projects } from './components/Projects';
+import { Resume } from './components/Resume';
+import { Contact } from './components/Contact';
 
 function App() {
   return (
     <div>
       <Navbar />
+      <AboutMe />
+      <Projects />
+      <Resume />
+      <Contact />
+
     </div>
   );
 }
