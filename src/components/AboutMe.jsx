c37a80f5e2dcd3edc090a811a80776ee89ccd471
meislifka <meislifka@gmail.com>
2024-01-20 06:29:09 -0500

    Added about me and projs

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -6,8 +6,24 @@ export const AboutMe = () => {
 
       <div id="about" className='bg-[#0001]'>
         <h1 className='text-3xl font-bold text-[#9ae3f5]'>About Me</h1>
-        I am a software engineer working at L3Harris. I've always had a passion for tinkering with things.
-        I started programming on a Mac ___. In high school, my passion for coding continued and ..... I enjoy building dlfafadsfdsf.
+        I am a software engineer currently working at L3Harris. I hold a BS degree from Villanova University in Computer Engineering with Minors in Cyber Security and Computer Science.
+
+        Journey into Programming:
+
+        I first dabbled began on my brother's old Macbook, running macOS Snow Leopard, and a "Learn To Program" book. In high school I took my first programming classes. I also disassembled an old laptop out of my own curiosity, fashioning it into an external monitor. I also repurposed the USB port from the laptop to fix a cord to a speaker.
+        Dispite my obvious interest in computers, I entered Villanova declared as a Mechanical Engineer, upon the end of my freshman year I realized my real passion was with computers so I switched majors.
+
+        Learning and Growth:
+
+        I firmly believe in the power of continuous learning. It's the key to avoiding stagnation and propelling progress.
+        I also stand by the notition that you should follow your passions. If you're not doing something that you're passionate about then the work seems meaningless. I learned this when I decided to swtich majors.
+
+
+        Values and Vision:
+
+        One of my core values is the importance of continuous learning. I thrive on acquiring new skills and knowledge, always seeking ways to enhance my abilities. Looking ahead, my goal is to continue pushing boundaries and contributing to the community.
+
+        Thanks for visiting my website and getting to know a bit about me.
       </div>
 
     </div >
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,18 +8,22 @@ export const Projects = () => {
         <h1 className='text-3xl font-bold text-[#9ae3f5]'>Projects</h1>
       </div>
       <div className='flex max-w-[1240px] mx-auto'>
-        <div className='grid md:grid-cols-3 gap-8 w-full'>
+        <div className='grid md:grid-cols-2 gap-8 w-full'>
           <div className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
             <img className='w-20 mx-auto mt-[-3rem] bg-white' src={placceholder} alt="/" />
-            <h2 className='text-2xl font-bold text-center py-8'>Stocks Higher or Lower Game</h2>
+            <h2 className='text-2xl font-bold text-center py-8'>SeeLion</h2>
           </div>
           <div className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
             <img className='w-20 mx-auto mt-[-3rem] bg-transparent' src={placceholder} alt="/" />
-            <h2 className='text-2xl font-bold text-center py-8'>Project 2</h2>
+            <h2 className='text-2xl font-bold text-center py-8'>Stocks Higher or Lower Game</h2>
           </div>
           <div className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
             <img className='w-20 mx-auto mt-[-3rem] bg-white' src={placceholder} alt="/" />
-            <h2 className='text-2xl font-bold text-center py-8'>Project 3</h2>
+            <h2 className='text-2xl font-bold text-center py-8'>Astriods</h2>
+          </div>
+          <div className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
+            <img className='w-20 mx-auto mt-[-3rem] bg-white' src={placceholder} alt="/" />
+            <h2 className='text-2xl font-bold text-center py-8'>Astriods</h2>
           </div>
         </div>
       </div>
